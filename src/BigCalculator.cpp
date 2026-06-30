1e28267cc78ba53f692e37ccea29f6490e44765f
Anna_Martynenko <martynenko7372@mail.ru>
2026-06-30 22:37:04 +0300

    Add implementation of methods

--- src/BigCalculator.cpp
+++ src/BigCalculator.cpp
@@ -1,1 +1,29 @@
 #include "BigCalculator.h"
+
+std::vector<char> BigCalculator::add(const std::vector<char>& num1, const std::vector<char>& num2) {
+    return std::vector<char>();
+}
+
+std::vector<char> subtract(const std::vector<char>& num1, const std::vector<char>& num2){
+    return std::vector<char>();
+}
+
+std::vector<char> multiply(const std::vector<char>& num1, const std::vector<char>& num2){
+    return std::vector<char>();
+}
+
+std::string division(const std::vector<char>& num1, const std::vector<char>& num2){
+    return std::string();
+}
+
+std::string pow(const std::vector<char>& num1, const std::vector<char>& num2){
+    return std::string();
+}
+
+std::vector<char> gcd(const std::vector<char>& num1, const std::vector<char>& num2){
+    return std::vector<char>();
+}
+
+std::vector<char> lcm(const std::vector<char>& num1, const std::vector<char>& num2){
+    return std::vector<char>();
+}
--- src/BigCalculator.h
+++ src/BigCalculator.h
@@ -11,8 +11,7 @@ public:
     // или просто перечислить сигнатуры методов, которые должны быть реализованы студентом:
     // add, subtract, multiply, division, pow, gcd, lcm
     // принимают (const std::vector<char>&, const std::vector<char>&) и возвращают std::string или std::pair<std::string, std::string> для division
-    // testing tests again 3
-    std::vector<char> add(const std::vector<char>& num1, const std::vector<char>& num);
+    std::vector<char> add(const std::vector<char>& num1, const std::vector<char>& num2);
     std::vector<char> subtract(const std::vector<char>& num1, const std::vector<char>& num2);
     std::vector<char> multiply(const std::vector<char>& num1, const std::vector<char>& num2);
     std::string division(const std::vector<char>& num1, const std::vector<char>& num2);
