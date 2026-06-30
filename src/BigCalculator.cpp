9244c44d7a710b2683c891a94a1e6dd8796e644f
Anna_Martynenko <martynenko7372@mail.ru>
2026-06-30 22:47:28 +0300

    fix error building

--- src/BigCalculator.cpp
+++ src/BigCalculator.cpp
@@ -1,29 +1,31 @@
 #include "BigCalculator.h"
 
+BigCalculator::BigCalculator() : normalSize(true) {}
+
 std::vector<char> BigCalculator::add(const std::vector<char>& num1, const std::vector<char>& num2) {
     return std::vector<char>();
 }
 
-std::vector<char> subtract(const std::vector<char>& num1, const std::vector<char>& num2){
+std::vector<char> BigCalculator::subtract(const std::vector<char>& num1, const std::vector<char>& num2) {
     return std::vector<char>();
 }
 
-std::vector<char> multiply(const std::vector<char>& num1, const std::vector<char>& num2){
+std::vector<char> BigCalculator::multiply(const std::vector<char>& num1, const std::vector<char>& num2){
     return std::vector<char>();
 }
 
-std::string division(const std::vector<char>& num1, const std::vector<char>& num2){
+std::string BigCalculator::division(const std::vector<char>& num1, const std::vector<char>& num2){
     return std::string();
 }
 
-std::string pow(const std::vector<char>& num1, const std::vector<char>& num2){
+std::string BigCalculator::pow(const std::vector<char>& num1, const std::vector<char>& num2){
     return std::string();
 }
 
-std::vector<char> gcd(const std::vector<char>& num1, const std::vector<char>& num2){
+std::vector<char> BigCalculator::gcd(const std::vector<char>& num1, const std::vector<char>& num2){
     return std::vector<char>();
 }
 
-std::vector<char> lcm(const std::vector<char>& num1, const std::vector<char>& num2){
+std::vector<char> BigCalculator::lcm(const std::vector<char>& num1, const std::vector<char>& num2){
     return std::vector<char>();
 }
