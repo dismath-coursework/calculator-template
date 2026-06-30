8fdcd344c43623485d6fff05e2c14bdf95438b44
Anna_Martynenko <martynenko7372@mail.ru>
2026-06-30 11:02:30 +0300

    testing tests attempt 2

--- src/BigCalculator.h
+++ src/BigCalculator.h
@@ -11,7 +11,7 @@ public:
     // или просто перечислить сигнатуры методов, которые должны быть реализованы студентом:
     // add, subtract, multiply, division, pow, gcd, lcm
     // принимают (const std::vector<char>&, const std::vector<char>&) и возвращают std::string или std::pair<std::string, std::string> для division
-    // testing tests
+    // testing tests again
     std::vector<char> add(const std::vector<char>& num1, const std::vector<char>& num);
     std::vector<char> subtract(const std::vector<char>& num1, const std::vector<char>& num2);
     std::vector<char> multiply(const std::vector<char>& num1, const std::vector<char>& num2);
