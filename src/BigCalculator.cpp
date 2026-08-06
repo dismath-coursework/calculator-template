ec67a17f586aea53a92b36923ee4d153590ed679
Anna_Martynenko <martynenko7372@mail.ru>
2026-08-06 23:21:06 +1000

    some changes

--- src/BigCalculator.cpp
+++ src/BigCalculator.cpp
@@ -1,6 +1,6 @@
 #include "BigCalculator.h"
 
-BigCalculator::BigCalculator() : normalSize(true) {}
+BigCalculator::BigCalculator() {}
 
 std::vector<char> BigCalculator::add(const std::vector<char>& num1, const std::vector<char>& num2) {
     return std::vector<char>();
--- src/BigCalculator.h
+++ src/BigCalculator.h
@@ -6,11 +6,7 @@ class BigCalculator
 {
 public:
     BigCalculator();
-    bool normalSize; // true, когда нет превышения 8 разрядов 
-
-    // или просто перечислить сигнатуры методов, которые должны быть реализованы студентом:
-    // add, subtract, multiply, division, pow, gcd, lcm
-    // принимают (const std::vector<char>&, const std::vector<char>&) и возвращают std::string или std::pair<std::string, std::string> для division
+ 
     std::vector<char> add(const std::vector<char>& num1, const std::vector<char>& num2);
     std::vector<char> subtract(const std::vector<char>& num1, const std::vector<char>& num2);
     std::vector<char> multiply(const std::vector<char>& num1, const std::vector<char>& num2);
